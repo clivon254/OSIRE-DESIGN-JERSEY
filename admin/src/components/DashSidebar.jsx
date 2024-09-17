4befcb0bd3040acc8d72b76d4f777ebec8416028
clivon254 <163561914+clivon254@users.noreply.github.com>
2024-09-17 17:49:29 +0300

    goons

--- admin/src/components/DashSidebar.jsx
+++ admin/src/components/DashSidebar.jsx
@@ -1,107 +1,106 @@
 
 
 
-// import React from 'react'
-// import { useContext } from 'react'
-// import { StoreContext } from '../context/store'
-// import { Link, useNavigate } from 'react-router-dom'
-// import { Sidebar } from 'flowbite-react'
-// import { HiAdjustments, HiDatabase, HiDocumentAdd, HiLogout, HiViewList } from "react-icons/hi"
-// import axios from "axios"
+import React from 'react'
+import { useContext } from 'react'
+import { StoreContext } from '../context/store'
+import { Link } from 'react-router-dom'
+import { Sidebar } from 'flowbite-react'
+import { HiAdjustments, HiDatabase, HiDocumentAdd, HiLogout, HiViewList } from "react-icons/hi"
+import axios from "axios"
 
 export default function DashSidebar() {
 
 
-    // const {url} = useContext(StoreContext)
+    const {url} = useContext(StoreContext)
 
   return (
 
-      <div>Hello</div>
 
-    // <Sidebar className="w-full h-full mt-10">
+    <Sidebar className="w-full h-full mt-10">
 
-    //     <Sidebar.Items>
+        <Sidebar.Items>
 
-    //         <Sidebar.ItemGroup className="">
+            <Sidebar.ItemGroup className="">
 
-    //             <div className="flex flex-col gap-y-2">
+                <div className="flex flex-col gap-y-2">
 
-    //                 <Link to="/">
+                    <Link to="/">
 
-    //                     <Sidebar.Item
-    //                       icon={HiDatabase}
-    //                       as="div"
-    //                       active={window.location.pathname === "/"}
-    //                     >
-    //                         DashBoard
-    //                     </Sidebar.Item>
+                        <Sidebar.Item
+                          icon={HiDatabase}
+                          as="div"
+                          active={window.location.pathname === "/"}
+                        >
+                            DashBoard
+                        </Sidebar.Item>
 
-    //                 </Link>
+                    </Link>
 
-    //                 <Link to="/list-products">
+                    <Link to="/list-products">
 
-    //                     <Sidebar.Item
-    //                       icon={HiViewList}
-    //                       as="div"
-    //                       active={window.location.pathname === "/list-products"}
-    //                     >
-    //                        List of products
-    //                     </Sidebar.Item>
+                        <Sidebar.Item
+                          icon={HiViewList}
+                          as="div"
+                          active={window.location.pathname === "/list-products"}
+                        >
+                           List of products
+                        </Sidebar.Item>
 
-    //                 </Link>
+                    </Link>
 
-    //                 <Link to="/list-orders">
+                    <Link to="/list-orders">
 
-    //                     <Sidebar.Item
-    //                       icon={HiAdjustments}
-    //                       as="div"
-    //                       active={window.location.pathname === "/list-orders"}
-    //                     >
-    //                         List of orders
-    //                     </Sidebar.Item>
+                        <Sidebar.Item
+                          icon={HiAdjustments}
+                          as="div"
+                          active={window.location.pathname === "/list-orders"}
+                        >
+                            List of orders
+                        </Sidebar.Item>
 
-    //                 </Link>
+                    </Link>
 
-    //                 <Link to="/add-product">
+                    <Link to="/add-product">
 
-    //                     <Sidebar.Item
-    //                       icon={HiDocumentAdd}
-    //                       as="div"
-    //                       active={window.location.pathname === "/add-product"}
-    //                     >
-    //                         Add product
-    //                     </Sidebar.Item>
+                        <Sidebar.Item
+                          icon={HiDocumentAdd}
+                          as="div"
+                          active={window.location.pathname === "/add-product"}
+                        >
+                            Add product
+                        </Sidebar.Item>
 
-    //                 </Link>
+                    </Link>
 
-    //                 <Link to="/coupon">
+                    <Link to="/coupon">
 
-    //                     <Sidebar.Item
-    //                       icon={HiDocumentAdd}
-    //                       as="div"
-    //                       active={window.location.pathname === "/coupon"}
-    //                     >
-    //                         coupon
-    //                     </Sidebar.Item>
+                        <Sidebar.Item
+                          icon={HiDocumentAdd}
+                          as="div"
+                          active={window.location.pathname === "/coupon"}
+                        >
+                            coupon
+                        </Sidebar.Item>
 
-    //                 </Link>
+                    </Link>
 
-    //                 <Sidebar.Item
-    //                       icon={HiLogout}
-    //                       as="div"
-    //                       onClick={handleSignOut}
-    //                       className="cursor-pointer"
-    //                 >
-    //                     sign out
-    //                 </Sidebar.Item>
+                    <Sidebar.Item
+                          icon={HiLogout}
+                          as="div"
+                          onClick={handleSignOut}
+                          className="cursor-pointer"
+                    >
+                        sign out
+                    </Sidebar.Item>
 
-    //             </div>
+                </div>
 
-    //         </Sidebar.ItemGroup>
+            </Sidebar.ItemGroup>
 
-    //     </Sidebar.Items>
+        </Sidebar.Items>
 
-    // </Sidebar>
+    </Sidebar>
 
   )
 
